3c4dc1c1b9c1445313329e6cb5423960494c6e86
Jason Boyles <jason@duncancreek.net>
2014-08-14 16:25:16 -0500

    add tests

---
+++ test/fabric/minecraft.py
@@ -0,0 +1,21 @@
+from fabric.api import env, run, task
+from envassert import detect, file, group, package, port, process, service, \
+    user
+
+
+@task
+def check():
+    env.platform_family = detect.detect()
+
+    assert file.exists('/srv/minecraft/minecraft_server.1.7.9.jar'), \
+        '/srv/minecraft/minecraft_server.1.7.9.jar does not exist'
+
+    assert port.is_listening(25565), 'port 25565/java is not listening'
+
+    assert user.exists('mcserver'), 'user mcserver does not exist'
+
+    assert group.is_exists('mcserver'), 'group mcserver does not exist'
+
+    assert process.is_up('java'), 'process java is not running'
+
+    assert service.is_enabled('minecraft'), 'service minecraft is not enabled'
